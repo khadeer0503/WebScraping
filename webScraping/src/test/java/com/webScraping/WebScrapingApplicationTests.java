d89d220eec9561c6a4ae0ed483ad2165c163d266
Khadeer <khadeer0503@gmail.com>
2024-09-26 14:08:15 +0200

    Demo Project for WebScraping...

---
+++ webScraping/src/test/java/com/webScraping/WebScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webScraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
